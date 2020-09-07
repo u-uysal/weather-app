a68790bc2265dd240d0bbe5cce00958c285e8a72
ufuk <ufuk.uysl5@gmail.com>
2020-09-07 18:14:03 +0200

    serving html

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello!");
+  res.send("<h1>Serving Html</h1>");
 });
 
 app.get("/help", (req, res) => {
